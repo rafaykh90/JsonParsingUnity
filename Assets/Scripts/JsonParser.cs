01126c62e8ed3806f2490d70ba2a8cb553364bd5
Khan Muhammad Rafay <khanm11@niksula.hut.fi>
2016-10-19 00:58:26 +0300

    Minor Changes

--- Assets/Scripts/JsonParser.cs
+++ Assets/Scripts/JsonParser.cs
@@ -4,14 +4,20 @@ using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
 using UnityEngine.Networking;
-using LitJson;
+using LitJson; //LitJson is a JSON library for parsing JSON
 
-//using UnityEngine.Experimental.Networking;
-
+/// <summary>
+/// Json parser.
+/// </summary>
 public class JsonParser {
 	static List<String> dataArray = new List<string>();
 
-	//Retrieves the Finnish Title from the JsonData Object which is recieved from the UIHandler class and stored them in a list then finally returns the list to the UIHandler class
+	/// <summary>
+	/// Retrieves the Finnish Title from the JsonData Object which is recieved from the UIHandler class 
+	/// and stores them in a list then finally returns the list to the UIHandler class
+	/// </summary>
+	/// <returns>List of titles</returns>
+	/// <param name="itemData">JsonData Object</param>
 	public static List<String> GetData (JsonData itemData){
 		dataArray.Clear ();
 		for (int i = 0; i < itemData["data"].Count; i++) {
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -6,6 +6,9 @@ using System;
 using LitJson;
 using UnityEngine.Networking;
 
+/// <summary>
+/// The purpose of this class is to get JSON string from the API server and perform UI action in the scene
+/// </summary>
 public class UIHandler : MonoBehaviour {
 	
 	public Text programInput, ErrorMessage;
@@ -14,14 +17,68 @@ public class UIHandler : MonoBehaviour {
 	int currentOffset = 0;
 	int currentLimit = 10;
 	string JsonString;
-	private String Url;
 	private String program;
 
-	// Use this for initialization
-	void Start(){
+	/// <summary>
+	/// OnClick Event for Search Button - Called from the scene
+	/// </summary>
+	public void SearchBtnPressed(){
+		//Remove all items from scrollview to populate with new data.
+		foreach (Transform child in ContentBox.transform) {
+			if (child.tag != "firstPos") {
+				Destroy (child.gameObject);
+			}
+		}
+		ContentBox.GetComponent<RectTransform> ().sizeDelta = new Vector2 (0, 0);
+		currentOffset = 0;
+		program = programInput.text.ToString ();
+		Search ();
 	}
 
-	//Adding Titles to the Scrollable List
+	/// <summary>
+	/// This method creates a URL string and calls the 
+	/// coroutine to get JSON data from the api server.
+	/// </summary>
+	public void Search(){
+		string Url = "http://external.api.yle.fi/v1/programs/items.json?type=" + program + "&offset=" + currentOffset.ToString() + "&limit=" +
+			currentLimit +"&app_id=2621b415&app_key=4c4114300a84dd6a177d498085429dc2";
+		print (Url);
+		StartCoroutine (GetText (Url));
+	}
+
+
+	/// <summary>
+	/// This Coroutine gets the JSON text from the server 
+	/// and call the AddItems method
+	/// </summary>
+	IEnumerator GetText (string Url) {
+		UnityWebRequest www = UnityWebRequest.Get (Url);
+		yield return www.Send ();
+		try {
+			if (www.isError) {
+				ErrorMessage.text = www.error;
+				ErrorMessage.gameObject.SetActive(true);
+				StartCoroutine (DisableErrorText());
+			} else {
+				//Json fetched
+				JsonString = www.downloadHandler.text.ToString ();
+				JsonData itemData = JsonMapper.ToObject (JsonString); //JsonMapper is a method from LitJson library which converts string into parsable JsonData object
+				dataArray.Clear ();
+				dataArray = JsonParser.GetData (itemData); //GetData method from the JsonParser script is called to get the desired data
+				AddItems ();
+				currentOffset += currentLimit; //Offset Update to get the next 10 items
+			}
+
+		} catch (Exception ex) {
+			ErrorMessage.text = ex.Message;
+			ErrorMessage.gameObject.SetActive(true);
+			StartCoroutine (DisableErrorText());
+		}
+	}
+
+	/// <summary>
+	/// Adding Titles to the Scrollable List and Resize the scrollview container
+	/// </summary>
 	public void AddItems() {
 		for (int i = 0; i < 10; i++) {
 			if (i == 0 && ContentBox.transform.childCount <= 1){
@@ -38,56 +95,10 @@ public class UIHandler : MonoBehaviour {
 		ContentBox.GetComponent<RectTransform> ().sizeDelta = new Vector2 (0, ContentBox.GetComponent<RectTransform>().rect.height + 495);
 	}
 
-	//OnClick Event for Search Button - Called from the scene
-	public void SearchBtnPressed(){
-		foreach (Transform child in ContentBox.transform) {
-			if (child.tag != "firstPos") {
-				Destroy (child.gameObject);
-			}
-		}
-		ContentBox.GetComponent<RectTransform> ().sizeDelta = new Vector2 (0, 0);
-		currentOffset = 0;
-		program = programInput.text.ToString ();
-		Search ();
-	}
-
-	//This method creates a URL string and calls the coroutine to get JSON data from the api server.
-	public void Search(){
-		Url = "http://external.api.yle.fi/v1/programs/items.json?type=" + program + "&offset=" + currentOffset.ToString() + "&limit=" +
-			currentLimit +"&app_id=2621b415&app_key=4c4114300a84dd6a177d498085429dc2";
-		print (Url);
-		StartCoroutine (GetText ());
-	}
-
-
-	//This Coroutine gets the JSON text from the server and 
-	IEnumerator GetText () {
-		UnityWebRequest www = UnityWebRequest.Get (Url);
-		yield return www.Send ();
-		try {
-			if (www.isError) {
-				ErrorMessage.text = www.error;
-				ErrorMessage.gameObject.SetActive(true);
-				StartCoroutine (DisableErrorText());
-			} else {
-				//Json fetched
-				JsonString = www.downloadHandler.text.ToString ();
-				JsonData itemData = JsonMapper.ToObject (JsonString);
-				dataArray.Clear ();
-				dataArray = JsonParser.GetData (itemData);
-				AddItems ();
-				currentOffset += currentLimit;
-			}
-
-		} catch (Exception ex) {
-			ErrorMessage.text = ex.Message;
-			ErrorMessage.gameObject.SetActive(true);
-			StartCoroutine (DisableErrorText());
-		}
-	}
-
-
-	//Checks if the vertical scrollbar is at the bottom then it will call the Search method to get the Next 10 items.
+	/// <summary>
+	/// Checks if the vertical scrollbar is at the bottom 
+	/// then it will call the Search method to get the Next 10 items.
+	/// </summary>
 	public void ScrollerValueChanged(Scrollbar ScrollVert){
 		if (ScrollVert.value == 0) {
 			Search ();
@@ -95,7 +106,10 @@ public class UIHandler : MonoBehaviour {
 	}
 
 
-	//If an error occurs in the Search or if the data is not find in the directory, an error message will be displayed in the the scene. This coroutine is used to disable the error text after 3 seconds.
+	/// <summary>
+	/// If an error occurs in the Search or if the data is not find in the directory, an error message will be displayed in the the scene. 
+	/// This coroutine is used to disable the error text after 3 seconds.
+	/// </summary>
 	IEnumerator DisableErrorText(){
 		yield return new WaitForSeconds (3);
 		ErrorMessage.gameObject.SetActive (false);
